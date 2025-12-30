7a59e698f658fb4bbfad4769ef31d5e393c3ebf2
Kadala Emmanuel <63037364+Kadala43@users.noreply.github.com>
2025-12-30 16:19:37 +0100

    Fix parameter name for season_type in player dashboard

--- bot.py
+++ bot.py
@@ -157,8 +157,8 @@ def get_player_data(name, season="2024-25"):
 
 def get_player_advanced(player_id, season="2024-25"):
     dash = playerdashboardbygeneralsplits.PlayerDashboardByGeneralSplits(
-        player_id=player_id, season=season, season_type_all_star="Regular Season"
-    )
+    player_id=player_id, season=season, season_type="Regular Season"
+)
     df = dash.overall_player_dashboard.get_data_frame()
     return df[["DEF_RATING", "PACE", "USG_PCT"]].iloc[0]
 
